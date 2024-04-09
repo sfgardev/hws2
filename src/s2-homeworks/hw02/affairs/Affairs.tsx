df90bf2c247a5248da81a9047663870b50fdd283
sfgardev <sf.garol@gmail.com>
2024-04-09 17:07:25 +0700

    refactoring affairs

--- src/s2-homeworks/hw02/affairs/Affairs.tsx
+++ src/s2-homeworks/hw02/affairs/Affairs.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { MouseEvent } from "react";
 import Affair from "./affair/Affair";
 import { AffairType, FilterType } from "../HW2";
 import s from "./Affairs.module.css";
@@ -16,30 +16,40 @@ type AffairsPropsType = {
 };
 
 function Affairs(props: AffairsPropsType) {
-  const setAll = () => {
-    props.setFilter("all");
-  };
-  const setHigh = () => {
-    props.setFilter("high");
-  };
-  const setMiddle = () => {
-    props.setFilter("middle");
-  };
-  const setLow = () => {
-    props.setFilter("low");
+  // const setAll = () => {
+  //   props.setFilter("all");
+  // };
+  // const setHigh = () => {
+  //   props.setFilter("high");
+  // };
+  // const setMiddle = () => {
+  //   props.setFilter("middle");
+  // };
+  // const setLow = () => {
+  //   props.setFilter("low");
+  // };
+
+  const setFilter = (event: MouseEvent<HTMLButtonElement>) => {
+    props.setFilter(event.currentTarget.value as FilterType);
   };
 
-  const cnAll =
-    s.button + " " + s.all + (props.filter === "all" ? " " + s.active : "");
-  const cnHigh =
-    s.button + " " + s.high + (props.filter === "high" ? " " + s.active : "");
-  const cnMiddle =
-    s.button +
-    " " +
-    s.middle +
-    (props.filter === "middle" ? " " + s.active : "");
-  const cnLow =
-    s.button + " " + s.low + (props.filter === "low" ? " " + s.active : "");
+  // const cnAll =
+  //   s.button + " " + s.all + (props.filter === "all" ? " " + s.active : "");
+  // const cnHigh =
+  //   s.button + " " + s.high + (props.filter === "high" ? " " + s.active : "");
+  // const cnMiddle =
+  //   s.button +
+  //   " " +
+  //   s.middle +
+  //   (props.filter === "middle" ? " " + s.active : "");
+  // const cnLow =
+  //   s.button + " " + s.low + (props.filter === "low" ? " " + s.active : "");
+
+  const setClasses = (filter: FilterType) => {
+    return `${s.button} ${s[filter]} ${
+      props.filter === filter ? s.active : ""
+    }`;
+  };
 
   const mappedAffairs = props.data.map((a: AffairType) => (
     <Affair
@@ -52,20 +62,36 @@ function Affairs(props: AffairsPropsType) {
   return (
     <div>
       <div className={s.buttonContainer}>
-        <button id={"hw2-button-all"} onClick={setAll} className={cnAll}>
+        <button
+          id={"hw2-button-all"}
+          value="all"
+          onClick={setFilter}
+          className={setClasses("all")}
+        >
           All
         </button>
-        <button id={"hw2-button-high"} onClick={setHigh} className={cnHigh}>
+        <button
+          id={"hw2-button-high"}
+          value="high"
+          onClick={setFilter}
+          className={setClasses("high")}
+        >
           High
         </button>
         <button
           id={"hw2-button-middle"}
-          onClick={setMiddle}
-          className={cnMiddle}
+          value="middle"
+          onClick={setFilter}
+          className={setClasses("middle")}
         >
           Middle
         </button>
-        <button id={"hw2-button-low"} onClick={setLow} className={cnLow}>
+        <button
+          id={"hw2-button-low"}
+          value="low"
+          onClick={setFilter}
+          className={setClasses("low")}
+        >
           Low
         </button>
       </div>
