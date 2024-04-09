c8047e3c1512973111c7d80f27650ad89f82adc6
sfgardev <sf.garol@gmail.com>
2024-04-09 18:45:20 +0700

    sprint 2 hw week 3

--- src/s2-homeworks/hw03/tests/pureAddUserCallback.test.tsx
+++ src/s2-homeworks/hw03/tests/pureAddUserCallback.test.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import {pureAddUserCallback} from '../HW3'
+import React from "react";
+import { pureAddUserCallback } from "../HW3";
 
-let initialState: any[]
+let initialState: any[];
 const setName = (a: any[]) => {
-    initialState = a
-}
+  initialState = a;
+};
 
 beforeEach(() => {
-    initialState = []
-})
+  initialState = [];
+});
 
-test('name 1', () => {
-    pureAddUserCallback('name', setName, initialState)
-    expect(initialState.length).toBe(1)
-    expect(initialState[0].name).toBe('name')
-    expect(!!initialState[0]._id).toBe(true)
-})
+test("name 1", () => {
+  pureAddUserCallback("name", setName, initialState);
+  expect(initialState.length).toBe(1);
+  expect(initialState[0].name).toBe("name");
+  expect(!!initialState[0]._id).toBe(true);
+});
