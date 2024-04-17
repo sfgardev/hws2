68fa420ab068dc44d254df1e812d60cced69a624
sfgardev <sf.garol@gmail.com>
2024-04-17 18:45:55 +0700

    sprint 2 hw week 4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -1,36 +1,47 @@
-import React, {ButtonHTMLAttributes, DetailedHTMLProps} from 'react'
-import s from './SuperButton.module.css'
+import React, { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
+import s from "./SuperButton.module.css";
 
 // тип пропсов обычной кнопки, children в котором храниться название кнопки там уже описан
-type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement>
+type DefaultButtonPropsType = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
 
 type SuperButtonPropsType = DefaultButtonPropsType & {
-    xType?: string
-}
+  xType?: string;
+};
 
-const SuperButton: React.FC<SuperButtonPropsType> = (
-    {
-        xType,
-        className,
-        disabled,
-        ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
-    }
-) => {
-    const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+const buttonVariants = {
+  default: s.default,
+  red: s.red,
+  secondary: s.secondary,
+};
 
-    return (
-        <button
-            disabled={disabled}
-            className={finalClassName}
-            {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
-        />
-    )
-}
+const SuperButton: React.FC<SuperButtonPropsType> = ({
+  xType = "default",
+  className,
+  disabled,
+  ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
+}) => {
+  const finalClassName =
+    s.button +
+    (disabled ? " " + s.disabled : "") +
+    (xType === "default" ? " " + s.default : "") +
+    (xType === "red" ? " " + s.red : "") +
+    (xType === "secondary" ? " " + s.secondary : "") +
+    // + (disabled
+    //         ? ...
+    //         : xType === 'red'
+    //             ? ...
+    (className ? " " + className : ""); // задачка на смешивание классов
 
-export default SuperButton
+  return (
+    <button
+      disabled={disabled}
+      className={finalClassName}
+      {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
+    />
+  );
+};
+
+export default SuperButton;
