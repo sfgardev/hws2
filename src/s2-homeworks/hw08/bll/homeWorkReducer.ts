fc71a737af595388cbced0cbbc1a4f31b22cfacb
sfgardev <sf.garol@gmail.com>
2024-05-16 14:41:04 +0700

    sprint 3 hw week 4

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,35 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType
+): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case "sort":
+      {
+        const stateCopy = [...state];
+        if (action.payload === "up") {
+          return stateCopy.sort((a, b) => {
+            if (a.name > b.name) {
+              return 1;
+            }
+            return -1;
+          });
+          //   return stateCopy.sort((a, b) => a.name.localeCompare(b.name));
         }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+        if (action.payload === "down")
+          return stateCopy.sort((a, b) => b.name.localeCompare(a.name));
+      }
+      return state;
+    case "check": {
+      return state.filter((user) => user.age >= action.payload); // need to fix
     }
-}
+    default:
+      return state;
+  }
+};
