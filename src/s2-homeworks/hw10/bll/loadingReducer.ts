4517e8789c3010f69dc876b458dfee50611957f5
sfgardev <sf.garol@gmail.com>
2024-06-04 14:06:14 +0700

    sprint 4 hw week 2

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,22 +1,31 @@
-const initState = {
-    isLoading: false,
-}
+const initState: LoadingStateType = {
+  isLoading: false,
+};
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // пишет студент  // need to fix
-
-        default:
-            return state
-    }
-}
+export const loadingReducer = (
+  state = initState,
+  action: LoadingActionType
+): LoadingStateType => {
+  // fix any
+  switch (action.type) {
+    // пишет студент  // need to fix
+    case "CHANGE_LOADING":
+      return { ...state, isLoading: action.isLoading };
+    default:
+      return state;
+  }
+};
 
 type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
-}
+  type: "CHANGE_LOADING";
+  isLoading: boolean;
+};
+
+export type LoadingStateType = {
+  isLoading: boolean;
+};
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
-    isLoading,
-})
+  type: "CHANGE_LOADING",
+  isLoading,
+});
