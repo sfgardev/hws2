03f53e2c33d2ceac5d1200247d040a735e99525b
sfgardev <sf.garol@gmail.com>
2024-06-17 13:13:55 +0700

    sprint 4 hw week 4

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,29 @@
-const initState = {
-    themeId: 1,
-}
+const initState: InitStateType = {
+  themeId: 1,
+};
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
+export const themeReducer = (
+  state = initState,
+  action: ChangeThemeIdActionType
+): InitStateType => {
+  // fix any
+  switch (action.type) {
+    // дописать
+    case "SET_THEME_ID":
+      return { ...state, themeId: action.id };
+    default:
+      return state;
+  }
+};
 
-        default:
-            return state
-    }
-}
+export const changeThemeId = (id: number): ChangeThemeIdActionType => ({
+  type: "SET_THEME_ID",
+  id,
+}); // fix any
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type InitStateType = {
+  themeId: number;
+};
+
+type ChangeThemeIdActionType = { type: "SET_THEME_ID"; id: number };
+// type ActionsType = ReturnType<typeof changeThemeId>
